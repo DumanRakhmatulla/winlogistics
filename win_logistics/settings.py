c894a073460246b64bd65362deb16bb2f58fe4cd
Zhandos04 <nurbekulyzhandos@gmail.com>
2025-04-19 02:16:34 +0500

    csrf_trusted_origins

--- win_logistics/settings.py
+++ win_logistics/settings.py
@@ -16,6 +16,8 @@ DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',')
+
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
